deb0b3597dbf730a1b417ae7791bc9b0d900f6ee
CHOCO <jh780465@naver.com>
2023-11-08 16:16:50 +0900

    [API] Simple API that returns a query parameter(apiNm) as it takes

--- src/main/java/com/chocochanel/hellospring/controller/HelloController.java
+++ src/main/java/com/chocochanel/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,13 @@ public class HelloController {
         System.out.println("LOGGER : HelloController.helloMvc ::: name = " + name);
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public String helloApi(@RequestParam(value = "apiNm", required = false) String apiNm) {
+        System.out.println("LOGGER : HelloController.helloApi ::: apiNm = " + apiNm);
+        // test url ::: localhost:8080/hello-api?apiNm=IF-API-026801
+        return apiNm;
     }
 }
