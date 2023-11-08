e69a58990dd053d074c62e28e9674d18f0330162
CHOCO <jh780465@naver.com>
2023-11-08 16:06:29 +0900

    [Thymeleaf] template response to '/hello-mvc' request with query parameter(name)

--- src/main/java/com/chocochanel/hellospring/controller/HelloController.java
+++ src/main/java/com/chocochanel/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.chocochanel.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,12 @@ public class HelloController {
         System.out.println("LOGGER : HelloController.hello");
         model.addAttribute("data", "Hello!");
         return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        System.out.println("LOGGER : HelloController.helloMvc ::: name = " + name);
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
